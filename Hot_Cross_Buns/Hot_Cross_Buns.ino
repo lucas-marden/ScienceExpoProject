db462d594ec8de591bddafe52fa8b429f0e9e6d9
Lucas Marden <lucas@marden.org>
2015-05-19 22:02:53 -0400

    Got generic loop running

--- Hot_Cross_Buns/Hot_Cross_Buns.ino
+++ Hot_Cross_Buns/Hot_Cross_Buns.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 
 #define NOTES_MAX 30
-String NOTES[NOTES_MAX] = { "D", "QR", "C", "QR", "Bb", "QR", "HR",  "D", "QR", "C", "QR", "Bb", "QR", "HR", "Bb","Bb","Bb","Bb", "C", "C", "C", "C", "D", "QR", "C", "QR", "Bb"};
+String NOTES[NOTES_MAX] = { "D", "QR", "C", "QR", "Bb", "QR", "HR",  "D", "QR", "C", "QR", "Bb", "QR", "HR", "Bb", "Bb", "Bb", "Bb", "C", "C", "C", "C", "D", "QR", "C", "QR", "Bb"};
 
 int moveTime = 600; // Milliseconds
 
@@ -14,11 +14,11 @@ int SERVO1_REST = 1450;
 int SERVO2_LEFT = 1400;
 int SERVO2_RIGHT = 1500;
 int SERVO2_REST = 1450;
-  
+
 Servo servo1;
 Servo servo2;
 
-void setup() 
+void setup()
 {
   servo1.attach(8);
   servo2.attach(9);
@@ -27,49 +27,62 @@ void setup()
 }
 
 void loop() {
- for(int i=0; i<NOTES_MAX; i++) {
+  for (int i = 0; i < NOTES_MAX; i++) {
+    // This is what note we will play during this part of the for() loop
     String theNote = NOTES[i];
+    Serial.println("I am going to play " + theNote);
+
+    // Now we are going to determine where servo1 and servo2 should go
     int servo1Position = SERVO1_REST;
     int servo2Position = SERVO2_REST;
-    // int restTime = 0;
+    int restTime = 0;
 
-    if(theNote == "D") {
+    if (theNote == "D") {
       servo1Position = SERVO1_LEFT;
-    } else if(theNote == "C") {
+    } else if (theNote == "C") {
       servo1Position = SERVO1_RIGHT;
-    } else if(theNote == "Bb") {
+    } else if (theNote == "Bb") {
       servo2Position = SERVO2_LEFT;
     }
-     //else if(theNote == "ER") {
-     // delay(measureSeconds/8 - moveTime);
-     else if(theNote == "QR") {
-      delay(measureSeconds/4 - moveTime);
-    } else if(theNote == "HR") {
-      delay(measureSeconds/2 - moveTime);
-    } else if(theNote == "WR") {
-      delay(measureSeconds - moveTime);
+    //else if(theNote == "ER") {
+    // delay(measureSeconds/8 - moveTime);
+    else if (theNote == "QR") {
+      restTime = measureSeconds / 4;
+    } else if (theNote == "HR") {
+      restTime = measureSeconds / 2;
+    } else if (theNote == "WR") {
+      restTime = measureSeconds;
     }
+
+    // Now we are going to use that information to
+    // move the servos
+    Serial.print("Moving servo1 to ");
+    Serial.println(servo1Position);
+    servo1.writeMicroseconds(servo1Position);
     
+    Serial.print("Moving servo2 to ");
+    Serial.println(servo2Position);
+    servo2.writeMicroseconds(servo2Position);
 
-
-    //if(restTime == 0) { // This is real note
+    if (restTime == 0) { // This is real note
       Serial.print("Sent servo location commands, sleep for ");
       Serial.print(moveTime);
       Serial.println(" milliseconds to wait for servos to arrive at location");
       delay(moveTime);
-      
-      if(servo1Position != SERVO1_REST) {
-        
+
+      if (servo1Position != SERVO1_REST) {
+        Serial.println("Moving servo1 to rest position");
         servo1.writeMicroseconds(SERVO1_REST);
       }
 
-      if(servo2Position != SERVO2_REST) {
-        
+      if (servo2Position != SERVO2_REST) {
+        Serial.println("Moving servo2 to rest position");
         servo2.writeMicroseconds(SERVO2_REST);
-      
+      }
+    } else { // This is a rest
+      if (restTime > moveTime) { 
+        delay(restTime - moveTime);
       }
     }
- }
-
-
-
+  }
+}
